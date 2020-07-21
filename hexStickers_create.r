92a9e9fb7fc11c6a747b20d070076e5aa31aa990
Ryo-N7 <ryonakagawara@gmail.com>
2020-07-21 18:00:30 +0900

    lots of stuff...

--- hexStickers_create.r
+++ hexStickers_create.r
@@ -1,5 +1,5 @@
 pacman::p_load(hexSticker, extrafont)
-loadfonts(device = "win")
+loadfonts(device = "win", quiet = TRUE)
 
 # Shego
 download.file("https://i.pinimg.com/originals/9c/49/c4/9c49c48b0d6d674e560df2891838a696.jpg",
@@ -31,3 +31,37 @@ sticker(subplot = paste0(tempdir() , ".png"), package = "tvthemes",
         s_width = 0.6, s_height = 0.6, p_color = "#8B4411", 
         h_fill = "#f5f5dc", h_color = "black",
         filename = "tvthems_hexsticker.png")
+
+sticker(subplot = "../../../Desktop/tvthemes extras/tv_boy_chikaku.png", 
+        package = "tvthemes",
+        p_size = 18, p_x = 1, p_y = 1.65, s_x = 1.0, s_y = .95,
+        s_width = 0.6, s_height = 0.6, p_color = "#8B4411", 
+        h_fill = "#f5f5dc", h_color = "black",
+        filename = "tvthemes_hexsticker_big.png")
+
+sticker(subplot = "../../../Desktop/tvthemes extras/tv_boy_chikaku.png", 
+        package = "tvthemes",
+        p_size = 17, p_x = 1, p_y = 1.65, p_family = "Garamond",
+        s_x = 1.0, s_y = .95,
+        s_width = 0.6, s_height = 0.6, p_color = "#8B4411", 
+        h_fill = "#f5f5dc", h_color = "black",
+        filename = "tvthemes_hexsticker_big.png")
+
+
+## bulletchartr
+download.file("https://i.imgur.com/AcSNPNi.png",
+              destfile = paste0(tempdir() , ".png"), mode="wb")
+#プロット
+sticker(subplot = paste0(tempdir() , ".png"), package = "bulletchartr",
+        p_size = 14, p_x = 1, p_y = 1.55, s_x = 1.0, s_y = .95,
+        s_width = 0.85, s_height = 0.85, p_color = "black", 
+        h_fill = "white", h_color = "red",
+        filename = "bulletchartr_hexsticker.png")
+
+## 2
+sticker(subplot = "../../../Desktop/bc_example_charts/bc_hex2.png", 
+        package = "bulletchartr",
+        p_size = 14, p_x = 1, p_y = 1.55, s_x = 1.0, s_y = .95,
+        s_width = 0.78, s_height = 0.75, p_color = "black", 
+        h_fill = "white", h_color = "red",
+        filename = "bulletchartr_hexsticker.png")
